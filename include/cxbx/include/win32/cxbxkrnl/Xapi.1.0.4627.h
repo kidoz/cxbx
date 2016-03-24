9b005fb44207c7b22e88ae4fa3e65b96f23dfc9e
kidoz <ckidoz@gmail.com>
2016-03-25 01:19:35 +0300

    Changed COPYING to LICENSE.md in comments

--- include/cxbx/include/win32/cxbxkrnl/Xapi.1.0.4627.h
+++ include/cxbx/include/win32/cxbxkrnl/Xapi.1.0.4627.h
@@ -22,7 +22,7 @@
 // *  GNU General Public License for more details.
 // *
 // *  You should have recieved a copy of the GNU General Public License
-// *  along with this program; see the file COPYING.
+// *  along with this program; see the file LICENSE.md.
 // *  If not, write to the Free Software Foundation, Inc.,
 // *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
 // *
