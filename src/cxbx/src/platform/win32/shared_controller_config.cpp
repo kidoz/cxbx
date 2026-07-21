e6311e49d09e6766fda623a4454844fb4158824b
Aleksandr Pavlov <ckidoz@gmail.com>
2026-07-21 18:07:55 +0300

    Narrowed input shared-state dependencies

---
+++ src/cxbx/src/platform/win32/shared_controller_config.cpp
@@ -0,0 +1,14 @@
+// Shared controller-configuration access for runtime consumers.
+#include "shared_controller_config.h"
+
+#include "EmuShared.h"
+
+namespace cxbx::platform
+{
+
+void GetSharedControllerConfig(XBController& controller)
+{
+    g_EmuShared->GetXBController(&controller);
+}
+
+} // namespace cxbx::platform
