57c238e5a1bd14f260de3476c071a5bdfd431010
Aleksandr Pavlov <ckidoz@gmail.com>
2026-07-22 01:02:16 +0300

    Standardized D3D8 conversion source filename

--- src/cxbx/src/win32/CxbxKrnl/d3d8_conversion.cpp
+++ src/cxbx/src/win32/CxbxKrnl/d3d8_conversion.cpp
@@ -7,7 +7,7 @@
 // *  `88bo,__,o,    oP"``"Yo,  _88o,,od8P   oP"``"Yo,  
 // *    "YUMMMMMP",m"       "Mm,""YUMMMP" ,m"       "Mm,
 // *
-// *   cxbx->win32->cxbxkrnl->EmuD3D8Conv.cpp
+// *   cxbx->win32->cxbxkrnl->d3d8_conversion.cpp
 // *
 // *  This file is part of the cxbx project.
 // *
