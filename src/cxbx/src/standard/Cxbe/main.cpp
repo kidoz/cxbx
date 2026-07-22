516ce189c4e913361dae3755beeb96a484105b16
Aleksandr Pavlov <ckidoz@gmail.com>
2026-07-22 14:36:08 +0300

    Standardized dormant CXBE entry point filename

--- src/cxbx/src/standard/Cxbe/main.cpp
+++ src/cxbx/src/standard/Cxbe/main.cpp
@@ -7,7 +7,7 @@
 // *  `88bo,__,o,    oP"``"Yo,  _88o,,od8P   oP"``"Yo,  
 // *    "YUMMMMMP",m"       "Mm,""YUMMMP" ,m"       "Mm,
 // *
-// *   cxbx->standard->cxbe->Main.cpp
+// *   cxbx->standard->cxbe->main.cpp
 // *
 // *  This file is part of the cxbx project.
 // *
