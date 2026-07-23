8cf5b8c8b2c800c1e3d7d31d0e3611c178be86d7
Aleksandr Pavlov <ckidoz@gmail.com>
2026-07-23 20:25:11 +0300

    Corrected Xbox ADPCM block decoding

--- include/cxbx/include/core/xbox_adpcm_decoder.h
+++ include/cxbx/include/core/xbox_adpcm_decoder.h
@@ -7,7 +7,20 @@
 
 namespace cxbx::audio
 {
-constexpr std::size_t kXboxAdpcmSamplesPerBlock = 64;
+constexpr std::size_t kXboxAdpcmEncodedBytesPerChannelBlock = 36;
+constexpr std::size_t kXboxAdpcmSamplesPerBlock = 65;
+
+[[nodiscard]] std::size_t XboxAdpcmEncodedBlockBytes(
+    std::size_t channels) noexcept;
+
+[[nodiscard]] std::size_t XboxAdpcmDecodedBlockBytes(
+    std::size_t channels) noexcept;
+
+[[nodiscard]] std::size_t XboxAdpcmGuestToPcmBytes(
+    std::size_t guestBytes, std::size_t channels) noexcept;
+
+[[nodiscard]] std::size_t XboxAdpcmPcmToGuestBytes(
+    std::size_t pcmBytes, std::size_t channels) noexcept;
 
 [[nodiscard]] std::size_t XboxAdpcmDecodedBytes(std::size_t sourceBytes,
                                                 std::size_t channels) noexcept;
